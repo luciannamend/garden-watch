4a3116c19765a85551f6b69178f75702091c28a8
Lucianna Mendonça Domingues <luciannamendoncadomingues@luciannas-mbp.home>
2025-05-25 17:52:20 -0400

    Initial commit

---
+++ garden-api/src/main/java/com/lucianna/gardenapi/GardenApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucianna.gardenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenApiApplication.class, args);
+	}
+
+}
---
+++ garden-api/src/test/java/com/lucianna/gardenapi/GardenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianna.gardenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
