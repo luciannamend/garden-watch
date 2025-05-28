e02e0e21fe608a9dd728fe0429c4fa367a2df5ed
Lucianna Mendonca <luciannamend@gmail.com>
2025-05-27 21:13:53 -0400

    Add module config

---
+++ garden-simulator/src/main/java/com/lucianna/gardensimulator/GardenSimulatorApplication.java
@@ -0,0 +1,16 @@
+package com.lucianna.gardensimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class GardenSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenSimulatorApplication.class, args);
+    }
+
+}
